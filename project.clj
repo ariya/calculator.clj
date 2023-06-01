a548c78be4c9909ca98427b9a6d6e310f50a7832
Ariya Hidayat <ariya.hidayat@gmail.com>
2023-06-01 18:58:48 +0000

    Revert the use of graal-build-time
    Unfortunately, for a reason not entirely clear yet, using
    graal-build-time leads to the execution error:
    
    ./calc "2 + (5 * 6 + 10)"
    Error: No matching method codePointCount found taking 2 args for class java.lang.String

--- project.clj
+++ project.clj
@@ -3,8 +3,7 @@
   :url "http://example.com/FIXME"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.0"]
-                 [com.github.clj-easy/graal-build-time "0.1.0"]]
+  :dependencies [[org.clojure/clojure "1.10.0"]]
   :main ^:skip-aot calculator.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
